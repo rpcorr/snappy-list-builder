1bf7f5eb217edce7c6640bf8d90a490058d2cd7f
Ronan Corr <ronan_corr@hotmail.com>
2022-04-15 17:14:19 -0400

    add a meta box to custom post type; create fields; ability to update field values

--- plugins/snappy-list-builder/snappy-list-builder.php
+++ plugins/snappy-list-builder/snappy-list-builder.php
@@ -129,12 +129,28 @@ function slb_add_subscriber_metaboxes( $post ) {
     );
 }
 
-add_action( 'add_meta_boxes_slb_subscriber', 'slb_add_subscriber_metaboxes');
+//add_action( 'add_meta_boxes_slb_subscriber', 'slb_add_subscriber_metaboxes');
 
 function slb_subscriber_metabox() {
+
+    global $post;
+
+    $post_id = $post->ID;
     
     wp_nonce_field( basename( __FILE__ ), 'slb_subscriber_nonce');
-        
+
+    $first_name = (!empty(get_post_meta($post_id, 'slb_first_name', true))) ? get_post_meta( $post_id, 'slb_first_name', true) : '';
+    $last_name = (!empty(get_post_meta($post_id, 'slb_last_name', true))) ? get_post_meta( $post_id, 'slb_last_name', true) : '';
+    $email = (!empty(get_post_meta($post_id, 'slb_email', true))) ? get_post_meta( $post_id, 'slb_email', true  ) : '';
+    $lists = (!empty(get_post_meta($post_id, 'slb_list', false))) ? get_post_meta( $post_id, 'slb_list', false) : [];
+
+    /*
+    echo '<br/>'. $first_name;
+    echo '<br/>'. $last_name;
+    echo '<br/>'. $email;
+    echo '<br/>'. var_dump($lists);
+    exit;
+    */
 ?>
 
 <style>
@@ -171,14 +187,16 @@ function slb_subscriber_metabox() {
     <div class="slb-field-container">
         <p>
             <label for="fName">First Name <span>*</span></label><br />
-            <input type="text" name="slb_first_name" id="fName" required="required" class="widefat">
+            <input type="text" name="slb_first_name" id="fName" required="required" class="widefat"
+                value="<?php echo $first_name; ?>">
         </p>
     </div>
 
     <div class=" slb-field-container">
         <p>
             <label for="lName">Last Name <span>*</span></label><br />
-            <input type="text" name="slb_last_name" id="lName" required="required" class="widefat">
+            <input type="text" name="slb_last_name" id="lName" required="required" class="widefat"
+                value="<?php echo $last_name; ?>">
         </p>
     </div>
 </div>
@@ -187,7 +205,8 @@ function slb_subscriber_metabox() {
     <div class="slb-field-container">
         <p>
             <label for="email">Email Address <span>*</span></label><br />
-            <input type="email" name="slb_email" id="email" required="required" class="widefat">
+            <input type="email" name="slb_email" id="email" required="required" class="widefat"
+                value="<?php echo $email; ?>">
         </p>
     </div>
 </div>
@@ -205,7 +224,10 @@ function slb_subscriber_metabox() {
                 if( !is_null( $list_query )) {
                     
                     foreach( $list_query as $list ) {
-                        echo '<li><label><input type="checkbox" name="slb_list[]" value="'. $list->ID .'" />'. $list->post_title .'</label></li>';
+
+                        $checked = ( in_array($list->ID, $lists) ) ? 'checked="checked"' : '';
+                        
+                        echo '<li><label><input type="checkbox" name="slb_list[]" value="'. $list->ID .'"'. $checked .' />'. $list->post_title .'</label></li>';
                     }
                 }
             ?>
@@ -238,10 +260,56 @@ function slb_save_slb_subscriber_meta( $post_id, $post) {
     $email = ( isset($_POST['slb_email'])) ? sanitize_text_field( $_POST['slb_email'] )  : '';
     $lists = ( isset($_POST['slb_list']) && is_array($_POST['slb_list'])) ? (array)$_POST['slb_list'] : [];
 
+    /*
     echo '<br/>'. $first_name;
     echo '<br/>'. $last_name;
     echo '<br/>'. $email;
-    echo '<br/>'. $lists;
+    echo '<br/>'. var_dump($lists);
     exit;
+    */
     
+    // update post meta
+    update_post_meta( $post_id, 'slb_first_name', $first_name);
+    update_post_meta( $post_id, 'slb_last_name', $last_name);
+    update_post_meta( $post_id, 'slb_email', $email );
+    
+    // delete the existing list meta for this post
+    delete_post_meta( $post_id , 'slb_list' );
+
+    // add new list meta
+    if (!empty($lists)) {
+        foreach( $lists as $index=>$list_id ) {
+        
+            // add list relational meta value
+            add_post_meta( $post_id, 'slb_list', $list_id, false ); // NOTE: NOT unique meta key
+        }
+    }
+}
+
+add_action('save_post', 'slb_save_slb_subscriber_meta', 10, 2);
+
+// add action to change post title
+function slb_edit_post_change_title(){
+    
+    global $post;
+    
+    if ($post->post_type == 'slb_subscriber') {
+        
+        add_filter(
+            'the_title',
+            'slb_subscriber_title',
+            100,
+            2
+        );
+    }
+}
+
+add_action( 'admin_head-edit.php','slb_edit_post_change_title');
+
+// function is called with in slb_edit_post_change_title to change post title to person's name
+function slb_subscriber_title( $title, $post_id) {
+    
+    $new_title = get_post_meta( $post_id, 'slb_first_name', true) .' '. get_post_meta( $post_id, 'slb_last_name', true);
+
+    return $new_title;
 }
