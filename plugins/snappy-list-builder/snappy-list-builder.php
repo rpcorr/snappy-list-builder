6f7b6a054219db9a2cb39a09c889c79162291fb2
Ronan Corr <ronan_corr@hotmail.com>
2022-04-15 01:56:09 -0400

    Setting up WordPress Plugin Headers

---
+++ plugins/snappy-list-builder/snappy-list-builder.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: Snappy List Builder
+Plugin URI: http:www.ronancorr.com/plugins/snappy-list-builder
+Description: The ultimate email list building plugin for WordPress. Captures new subscribers. Reward subscribers with a custom download upon opt-in.  Build unlimited lists. Import and export subscribers easily with .csv
+Version: 1.0
+Author: Ronan Corr
+Author URI: http://www.ronancorr.com
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: snappy-list-builder
+*/
